bbf4e923c3370af3ec64e0fa2f4358bc0a7ca17d
jhanvigoel <146558987+jhanvigoel@users.noreply.github.com>
2025-09-10 03:27:47 +0530

    Fix import path for AiController in aiRoutes

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/AiController.js";
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/aiController.js";
 import { upload } from "../config/multer.js";
 
 const aiRouter = express.Router();
